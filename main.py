611684119c00de5831c2759814952fd5fe0da0d4
Minimalon <grafxartyr@gmail.com>
2022-06-03 13:06:48 +0300

    test

--- main.py
+++ main.py
@@ -221,8 +221,8 @@ def send_last_file(message):
 
 def send_dates_files(message):
     # try:
-    #     regex = re.compile(r'[0-9]{1,4}')
-    #     if re.fullmatch(regex, message.text):
+        regex = re.compile(r'[0-9]{1,4}')
+        if re.fullmatch(regex, message.text):
             logger.info(f"Ввели номер компьютера '{message.text}'")
             cash = functions.check_repeat_cash(message.text)
             logger.info(f'check_repeat_cash нашел "{cashInfo.cash_number}"')
@@ -248,10 +248,10 @@ def send_dates_files(message):
             cashInfo.dates_files = cash_dates
 
             bot.send_message(message.chat.id, 'Выберите одну из дат:', reply_markup=markup)
-        # else:
-        #     logger.debug("Номер кассы введена не правильно - " + message.text)
-        #     bot.send_message(message.chat.id, 'Номер кассы введена не правильно')
-        #     start_select(message)
+        else:
+            logger.debug("Номер кассы введена не правильно - " + message.text)
+            bot.send_message(message.chat.id, 'Номер кассы введена не правильно')
+            start_select(message)
     # except Exception as ex:
     #     bot_error_send(message)
     #     logger.error(f'{ex} --- {cashInfo.cash_number}')
